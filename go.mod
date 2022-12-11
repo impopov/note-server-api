e6db2ebd103cd9c3ad177d32cd3ad2b8ec62ca29
impopov <impopovg@gmail.com>
2022-12-11 13:33:23 +0530

    lesson implementation

--- go.mod
+++ go.mod
@@ -9,6 +9,10 @@ require (
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
 	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
 	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	golang.org/x/text v0.4.0 // indirect
--- internal/app/api/note_v1/create_note.go
+++ internal/app/api/note_v1/create_note.go
@@ -5,6 +5,18 @@ import (
 	"fmt"
 
 	desc "github.com/impopov/note-server-api/pkg/note_v1"
+	_ "github.com/jackc/pgx/stdlib"
+	"github.com/jmoiron/sqlx"
+)
+
+const (
+	noteTable  = "note"
+	host       = "localhost"
+	port       = "54321"
+	dbUser     = "note-service-user"
+	dbPassword = "note-service-password"
+	dbName     = "note-service"
+	sslMode    = "disable"
 )
 
 func (n *Implementation) CreateNote(ctx context.Context, req *desc.CreateNoteRequest) (*desc.CreateNoteResponse, error) {
@@ -14,6 +26,17 @@ func (n *Implementation) CreateNote(ctx context.Context, req *desc.CreateNoteReq
 	fmt.Println("Title:", req.GetTitle())
 	fmt.Println("Text:", req.GetText())
 	fmt.Println("Author:", req.GetAuthor())
+
+	dbDSN := fmt.Sprintf(
+		"host=%s port=%s dbname=%s user=%s password=%ssslmode=%s",
+		host, port, dbName, dbUser, dbPassword, sslMode,
+	)
+
+	db, err := sqlx.Open("pgx", dbDSN)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
 
 	return &desc.CreateNoteResponse{
 		Id: 1,
